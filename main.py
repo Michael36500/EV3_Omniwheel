67318f40622e3a69b2e6c7f51264ed67b1b4587b
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-04 21:27:23 +0200

    more slow and steady progress

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ def run_m (lu, ru, rd, ld):
     
     m_lu.dc(lu)
     m_ru.dc(ru)
-    m_ru.dc(rd)
-    m_ru.dc(ld)
-    wait(2)
+    m_rd.dc(rd)
+    m_ld.dc(ld)
+    wait(2000)
 
-def stop_m (lu, ru, rd, ld):
+def stop_m ():
     global m_lu
     global m_ru
     global m_rd
     global m_ld
     
-    m_lu.stop()
-    m_ru.stop()
-    m_ru.stop()
-    m_ru.stop()
-    wait(2)
+    m_lu.hold()
+    m_ru.hold()
+    m_rd.hold()
+    m_ld.hold()
+    wait(2000)
 # rych = rychlost
 # def move(where, rych, kolik):
 #     global ev3
@@ -50,22 +50,23 @@ def stop_m (lu, ru, rd, ld):
 #     global m_ld
 
 
-rych = 50
-
-# rovně
-run_m (1 * rych, 1 * rych, -1 * rych, -1 * rych)
-stop_m()
+rych = 20
 
 # doleva
-run_m (-1 * rych, 1 * rych, 1 * rych, -1 * rych)
+run_m (20, -20, -20, 20)
+# run_m (-1 * rych, -1 * rych, 1 * rych, 1 * rych)
 stop_m()
 
-# dozadu
-run_m (-1 * rych, -1 * rych, 1 * rych, 1 * rych)
+# dopředu
+run_m (1 * rych, -1 * rych, -1 * rych, 1 * rych)
 stop_m()
 
 # doprava
-run_m (1 * rych, -1 * rych, -1 * rych, 1 * rych)
+run_m (1 * rych, 1 * rych, -1 * rych, -1 * rych)
+stop_m()
+
+# dozadu
+run_m (-1 * rych, 1 * rych, 1 * rych, -1 * rych)
 stop_m()
 
 
