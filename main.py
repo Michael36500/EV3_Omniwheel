e8688f16ffa0451aacca51610a27afd01b1b7219
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-05 12:17:41 +0200

    more tries

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pybricks.ev3devices import Motor, ColorSensor, TouchSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait, StopWatch
 
+import math
 
 # setup kostka, stopky
 ev3 = EV3Brick()
@@ -14,7 +15,8 @@ m_ru = Motor(Port.C)
 m_rd = Motor(Port.B)
 m_ld = Motor(Port.D)
 
-
+m_lu.dc(-30)
+wait(1000)
 # where = ve stupních, dokola
 # rychlost = rychlost
 # kolik = o kolik se posune (součet otočení všech motorů)
@@ -28,7 +30,7 @@ def run_m (lu, ru, rd, ld):
     m_ru.dc(ru)
     m_rd.dc(rd)
     m_ld.dc(ld)
-    wait(2000)
+    wait(1000)
 
 def stop_m ():
     global m_lu
@@ -42,31 +44,40 @@ def stop_m ():
     m_ld.hold()
     wait(500)
 # rych = rychlost
-# def move(where, rych, kolik):
-#     global ev3
-#     global m_lu
-#     global m_ru
-#     global m_rd
-#     global m_ld
+def move(angle, rych):
+    global m_lu
+    global m_ru
+    global m_rd
+    global m_ld
+
+    rychx = rych * math.cos(angle)
+    rychy = rych * math.sin(angle)
+    run_m (rychx + rychy, rychx - rychy, -1 * (rychx + rychy), -1 * (rychx - rychy))
+
+# def move(angle, rych):
+#     rychx =
+
+move(45, 30)
+# wait(5000)
+# x = 0
+# y = 20
+# while True:
+#     x = x % 40
+#     y = y % 40
+    
+#     x += 2
+#     y += 2
+
+#     print(x, y)
+
+#     run_m (x,y, x*-1, y*-1)
+
+#     wait(10)
+# rych = 20
+# while True:
+#     for phase in range (0, 1, 0.01):
+#         run_m(phase)
 
 
-rych = 100
-
-# dozadu
-run_m (-1 * rych, -1 * rych, 1 * rych, 1 * rych)
-stop_m()
-
-# doprava
-run_m (1 * rych, -1 * rych, -1 * rych, 1 * rych)
-stop_m()
-
-# dopředu
-run_m (1 * rych, 1 * rych, -1 * rych, -1 * rych)
-stop_m()
-
-# doleva
-run_m (-1 * rych, 1 * rych, 1 * rych, -1 * rych)
-stop_m()
-
-
+#         wait(100)
 
